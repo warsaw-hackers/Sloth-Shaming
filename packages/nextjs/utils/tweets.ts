1d3506c3c35209a45b2a9ab4df87b02f0a8e0fed
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-09-07 20:39:31 +0200

    added some really nice roasts

--- packages/nextjs/utils/tweets.ts
+++ packages/nextjs/utils/tweets.ts
@@ -7,6 +7,15 @@ export const exampleTweets = [
     "Got tired of waiting for payment, so I minted a shame token! 🦥 #SlothShaming #RequestNetwork",
     "If only their code was as slow as their payment... 🦥 #SlothShaming",
     "In hackathons, we run on caffeine, but some people run on turtle time. 🐢 Thank you #SlothShaming!",
-    "Another hackathon, another slow payer shamed! 🦥 #ETHWarsaw"
+    "Another hackathon, another slow payer shamed! 🦥 #ETHWarsaw",
+    "Waiting for ETH payments from you feels like watching a sloth learn to code Solidity... slow, painful, and makes me question if block confirmations are actually faster than your promises.",
+    "Still waiting on that ETH payment like a sloth with cold feet 🦥... Guess not everyone can move like wolves and cheetahs in the crypto wild 🐺⚡️🐆. At this rate, the blockchain’s gonna have gray hairs before the funds arrive ⏳💸",
+    "Finally, I can #slothshaming to name and shame the one ETH payer who's slower than gas fees on a congested network 🦥⛽️. While wolves are howling at moonlit transactions and cheetahs are zipping past confirmations, I'm here... still waiting ⏳💸.",
+    "ETH transfer so slow, I’m starting to wonder if the sloth's got fat fingers 🦥💸… or just no urgency at all.",
+    "Bro, even a sloth would've paid me faster than you. What are you doing, writing my ETH payment by hand? 🦥💸",
+    "You're the sloth of crypto payments, my guy 🦥💸. Send the ETH before the blockchain forgets we even had a deal.",
+    "I crushed the hackathon, but the only thing getting hacked is my patience waiting for your ETH payout 🦥⛓️.",
+    "I thought winning the bounty was the hard part, but waiting on you to pay up is a whole new level of endurance 🦥💸.",
+    "I won the bounty, but it looks like I need to code a time machine just to see that ETH arrive 🦥💸."
   ];
   
