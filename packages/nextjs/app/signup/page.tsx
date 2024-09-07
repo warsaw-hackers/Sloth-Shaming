7a8fc2fd3c33323ab9ab388698603aaac30db6f7
nhestrompia <nhestrompia@gmail.com>
2024-09-07 11:45:33 +0200

    fix: buld errors

--- packages/nextjs/app/signup/page.tsx
+++ packages/nextjs/app/signup/page.tsx
@@ -1,50 +1,53 @@
 "use client";
 
+import { useEffect, useState } from "react";
+import { run } from "~~/utils/golem";
+
 export default function Home() {
-  //   const [results, setResults] = useState<string[]>([]);
-  //   const [formData, setFormData] = useState({
-  //     yagnaAppKey: "",
-  //     yagnaApiBasePath: "",
-  //     subnetTag: "",
-  //     imageTag: "",
-  //     paymentNetwork: "",
-  //   });
+  const [results, setResults] = useState<string[]>([]);
+  const [formData, setFormData] = useState({
+    yagnaAppKey: "try_golem",
+    yagnaApiBasePath: "",
+    subnetTag: "",
+    imageTag: "",
+    paymentNetwork: "",
+  });
 
-  //   useEffect(() => {
-  //     // This line allows you to watch golem-js internal logs in the browser console!
-  //     if (typeof window !== "undefined") {
-  //       localStorage.debug = "golem-js:*";
-  //     }
-  //   }, []);
+  useEffect(() => {
+    // This line allows you to watch golem-js internal logs in the browser console!
+    if (typeof window !== "undefined") {
+      localStorage.debug = "golem-js:*";
+    }
+  }, []);
 
-  //   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  //     const { id, value } = e.target;
-  //     setFormData(prev => ({ ...prev, [id]: value }));
-  //   };
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = e.target;
+    setFormData(prev => ({ ...prev, [id]: value }));
+  };
 
-  //   const handleRun = async () => {
-  //     if (!formData.yagnaAppKey) {
-  //       alert("You didn't provide your Yagna AppKey");
-  //       return;
-  //     }
+  const handleRun = async () => {
+    if (!formData.yagnaAppKey) {
+      alert("You didn't provide your Yagna AppKey");
+      return;
+    }
 
-  //     await run(
-  //       {
-  //         key: formData.yagnaAppKey,
-  //         url: formData.yagnaApiBasePath,
-  //         subnetTag: formData.subnetTag,
-  //         imageTag: formData.imageTag,
-  //         network: formData.paymentNetwork,
-  //       },
-  //       result => {
-  //         setResults(prev => [...prev, result]);
-  //       },
-  //     );
-  //   };
+    await run(
+      {
+        key: formData.yagnaAppKey,
+        url: formData.yagnaApiBasePath,
+        subnetTag: formData.subnetTag,
+        imageTag: formData.imageTag,
+        network: formData.paymentNetwork,
+      },
+      result => {
+        setResults(prev => [...prev, result]);
+      },
+    );
+  };
 
   return (
     <div>
-      {/* <input id="yagnaAppKey" value={formData.yagnaAppKey} onChange={handleInputChange} placeholder="Yagna AppKey" />
+      <input id="yagnaAppKey" value={formData.yagnaAppKey} onChange={handleInputChange} placeholder="Yagna AppKey" />
       <input
         id="yagnaApiBasePath"
         value={formData.yagnaApiBasePath}
@@ -64,7 +67,7 @@ export default function Home() {
         {results.map((result, index) => (
           <li key={index}>{result}</li>
         ))}
-      </ul> */}
+      </ul>
       <div className="grid grid-cols-3 p-16 font-montserrat">
         <div className="flex flex-col">
           <h1 className="tracking-tighter text-2xl">Registration</h1>
