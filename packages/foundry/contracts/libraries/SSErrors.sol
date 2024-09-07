117b8d60e1651da767219542ac3fd1eed08c01a8
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-09-07 09:46:53 +0200

    more updates

--- packages/foundry/contracts/libraries/SSErrors.sol
+++ packages/foundry/contracts/libraries/SSErrors.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: AGPL-3.0
 pragma solidity 0.8.26;
 
 import {ERC721} from "@solady/contracts/tokens/ERC721.sol";
@@ -6,7 +6,7 @@ import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * @title Rep
- * @notice SlothShaming errors
+ * @notice Sloth Shaming errors
  * @author https://x.com/0xjsieth
  *
  */
