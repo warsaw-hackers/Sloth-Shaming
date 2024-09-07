cda957db157b3a82f94a304b45c71e881c050ee5
nhestrompia <nhestrompia@gmail.com>
2024-09-07 18:50:03 +0200

    feat: some ui changes

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -77,7 +77,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+    <div className="sticky lg:static top-0 navbar  text-white  min-h-0 flex-shrink-0 justify-between z-20 shadow-md px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
@@ -103,7 +103,7 @@ export const Header = () => {
         </div>
         <Link href="/" passHref className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0">
           <div className="flex relative w-10 h-10">
-            <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
+            <Image alt="Sloth Shame Logo" className="cursor-pointer" fill src="/assets/homepage-sloth.svg" />
           </div>
           <div className="flex flex-col">
             <span className="font-bold leading-tight">Sloth Shaming</span>
--- packages/nextjs/utils/scaffold-eth/getMetadata.ts
+++ packages/nextjs/utils/scaffold-eth/getMetadata.ts
@@ -8,7 +8,7 @@ const titleTemplate = "%s | Sloth Shaming";
 export const getMetadata = ({
   title,
   description,
-  imageRelativePath = "/thumbnail.jpg",
+  imageRelativePath = "/thumbnail.png",
 }: {
   title: string;
   description: string;
@@ -44,7 +44,7 @@ export const getMetadata = ({
       images: [imageUrl],
     },
     icons: {
-      icon: [{ url: "/favicon.png", sizes: "32x32", type: "image/png" }],
+      icon: [{ url: "/thumbnail.png", sizes: "32x32", type: "image/png" }],
     },
   };
 };
