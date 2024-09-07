a475bb72347cdde261e7ab086bf0714289d244e1
nhestrompia <nhestrompia@gmail.com>
2024-09-07 12:40:03 +0200

    feat: pinata integration

---
+++ packages/nextjs/hooks/usePinataRetrieveData.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+
+const GATEWAY = "https://lavender-manual-goat-205.mypinata.cloud";
+
+async function fetchFileFromIPFS(cid: string, walletAddress: string) {
+  const url = `${GATEWAY}/ipfs/${cid}`;
+  const response = await fetch(url, { next: { revalidate: 3600 } }); // Cache for 1 hour
+  console.log("🚀 ~ fetchFileFromIPFS ~ url:", url);
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.text();
+}
+
+export function usePinataRetrieveData(cid: string, walletAddress: string) {
+  return useQuery({
+    queryKey: ["pinataData", cid, walletAddress],
+    queryFn: () => fetchFileFromIPFS(cid, walletAddress),
+    enabled: !!walletAddress,
+  });
+}
