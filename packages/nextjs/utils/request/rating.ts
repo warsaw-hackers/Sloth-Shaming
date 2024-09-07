0a6373ff5fe4216fe284e494b9ffaf1df4020d04
jacksmithinsulander <666.jack.smith@protonmail.com>
2024-09-07 14:10:49 +0200

    handing it over to the homeless guy

--- packages/nextjs/utils/request/rating.ts
+++ packages/nextjs/utils/request/rating.ts
@@ -1,18 +1,19 @@
-// import { Types } from "@requestnetwork/request-client.js";
-// import { calculateStatus } from "./helper";
+import { calculateStatus } from "./helper";
+import { Types } from "@requestnetwork/request-client.js";
 
-// export const returnRating = (invoices: Types.IRequestDataWithEvents[]) => {
-// for (let i = 0; i < invoices.length; i++) {
-// const status = calculateStatus(
-// invoices[i].state,
-// BigInt(invoices[i].expectedAmount),
-// BigInt(invoices[i].balance?.balance || 0),
-// );
+export const returnRating = (invoices: Types.IRequestDataWithEvents[]) => {
+  for (let i = 0; i < invoices.length; i++) {
+    invoices[i].extensions
+    // const status = calculateStatus(
+      // invoices[i].state,
+      // BigInt(invoices[i].expectedAmount),
+      // BigInt(invoices[i].balance?.balance || 0),
+    // );
 
-// //invoices[i].events[0].name === "Paid"
+    //invoices[i].events[0].name === "Paid"
 
-// // if (status === "Paid") {
+    // if (status === "Paid") {
 
-// // }
-// }
-// }
+    // }
+  }
+};
